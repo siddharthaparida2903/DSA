4d52a03d644afc81c3009e8fc1b104fcc8d5c665
Siddhartha Parida <104245924+siddharthaparida2903@users.noreply.github.com>
2026-05-27 02:16:58 +0530

    Time: 3 ms (86.56%) | Memory: 21.2 MB (52.15%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,13 +1,24 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = {}
-        res = maxCount = 0
+        
+        ## BOYER-MOORE METHOD (O(1))
+        res, count = 0, 0
 
-        for num in nums:
-            count[num] = 1 + count.get(num, 0)
-            res = num if count[num] > maxCount else res
-            maxCount = max(count[num], maxCount)
+        for n in nums:
+            if count == 0:
+                res = n
+            count += (1 if n == res else -1)
         return res
+
+        ## HASH MAP METHOD (O(n))
+        # count = {}
+        # res = maxCount = 0
+
+        # for num in nums:
+        #     count[num] = 1 + count.get(num, 0)
+        #     res = num if count[num] > maxCount else res
+        #     maxCount = max(count[num], maxCount)
+        # return res
 
         ## SORTING METHOD
         # nums.sort()

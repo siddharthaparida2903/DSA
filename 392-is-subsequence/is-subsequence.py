4ed78fc8c6b3281902b06c48ea249bebdc2f475c
Siddhartha Parida <104245924+siddharthaparida2903@users.noreply.github.com>
2026-06-05 22:23:58 +0530

    Time: 15 ms (3.28%) | Memory: 20 MB (7.36%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        n, m = len(s), len(t)
+        dp = [[False] * (m + 1) for _ in range(n + 1)]
+
+        for j in range(m + 1):
+            dp[n][j] = True
+
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                if s[i] == t[j]:
+                    dp[i][j] = dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = dp[i][j + 1]
+
+        return dp[0][0]

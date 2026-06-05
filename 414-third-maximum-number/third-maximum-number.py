9a204b043bf6cc3066ba7dd7a8736421519f261d
Siddhartha Parida <104245924+siddharthaparida2903@users.noreply.github.com>
2026-06-05 22:30:08 +0530

    Time: 2 ms (35.82%) | Memory: 19.3 MB (99.90%) - LeetSync

---
+++ 414-third-maximum-number/third-maximum-number.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        first = second = third = None
+
+        for num in nums:
+            if num in (first, second, third):
+                continue
+
+            if first is None or num > first:
+                third = second
+                second = first
+                first = num
+            elif second is None or num > second:
+                third = second
+                second = num
+            elif third is None or num > third:
+                third = num
+
+        return third if third is not None else first

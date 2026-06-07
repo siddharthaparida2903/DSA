95175818720cb4a295e69268e5ebcd29e2522e34
Siddhartha Parida <104245924+siddharthaparida2903@users.noreply.github.com>
2026-06-07 11:35:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (62.25%) - LeetSync

---
+++ 520-detect-capital/detect-capital.py
@@ -0,0 +1,7 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        return (
+            word.isupper() or
+            word.islower() or
+            word.istitle()
+        )
